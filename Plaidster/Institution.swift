a849750117f709068aa9bd17868b582342514477
Willow Bumby <willow.rose@bumby.ca>
2016-01-13 21:49:41 -0800

    Add Institution struct

--- Plaidster/Institution.swift
+++ Plaidster/Institution.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+public struct Institution {
+    static let Amex = "amex"
+    static let Bofa = "bofa"
+    static let Capone360 = "capone360"
+    static let Schwab = "schwab"
+    static let Chase = "chase"
+    static let Citi = "citi"
+    static let Fidelity = "fidelity"
+    static let Navy = "nfcu"
+    static let PNC = "pnc"
+    static let SunTrust = "suntrust"
+    static let TDBank = "td"
+    static let US = "us"
+    static let USAA = "usaa"
+    static let Wells = "wells"
+}
