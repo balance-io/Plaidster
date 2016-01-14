3570f3b630758fd84ce0c74e7eaf338415fd80e9
Willow Bumby <willow.rose@bumby.ca>
2016-01-13 21:49:51 -0800

    Add main initialiser method

--- Plaidster/Plaidster.swift
+++ Plaidster/Plaidster.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+public enum PlaidEnvironment {
+    case Production
+    case Development
+}
+
+public struct Plaidster {
+    
+    // MARK: Constants
+    private static let DevelopmentBaseURL = "https://tartan.plaid.com/"
+    private static let ProductionBaseURL = "https://api.plaid.com/"
+    
+    // MARK: Properties
+    let clientID: String
+    let secret: String
+    let baseURL: NSURL
+    
+    // MARK: Initialisation
+    init(clientID: String, secret: String, mode: PlaidEnvironment) {
+        self.clientID = clientID
+        self.secret = secret
+        
+        switch mode {
+        case .Development:
+            self.baseURL = NSURL(string: Plaidster.DevelopmentBaseURL)!
+        case .Production:
+            self.baseURL = NSURL(string: Plaidster.ProductionBaseURL)!
+        }
+    }
+}
