f7aa8a81d191e2ec15ca72e4494c02d7f55c4c93
Willow Bumby <willow.rose@bumby.ca>
2016-01-13 16:58:26 -0800

    Add starter project

---
+++ Plaidster/Plaidster.h
@@ -0,0 +1,19 @@
+//
+//  Plaidster.h
+//  Plaidster
+//
+//  Created by Willow Bumby on 2016-01-13.
+//  Copyright © 2016 Willow Bumby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Plaidster.
+FOUNDATION_EXPORT double PlaidsterVersionNumber;
+
+//! Project version string for Plaidster.
+FOUNDATION_EXPORT const unsigned char PlaidsterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Plaidster/PublicHeader.h>
+
+
